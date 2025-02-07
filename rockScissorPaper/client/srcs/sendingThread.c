ab9cb1c374944a7c9ed923c10ea682f8aca2807e
yedididi <iyeji366@gmail.com>
2025-02-07 17:12:35 +0900

    main print

--- rockScissorPaper/client/srcs/sendingThread.c
+++ rockScissorPaper/client/srcs/sendingThread.c
@@ -58,12 +58,13 @@ t_gameInfo getClick(t_info *info, int touch_fd)
    for (;;)
    {
       int ret = read(touch_fd, &ev, sizeof(struct input_event));
+      printf("read ok\n");
       if (ret == -1)
       {
          printf("touchtest: %s (%d)\n", strerror(errno), __LINE__);
          exit(1);
       }
-
+      
       if (ev.type == 1 && ev.code == 330 && ev.value == 0)
       {
          printf("pressed\n");
